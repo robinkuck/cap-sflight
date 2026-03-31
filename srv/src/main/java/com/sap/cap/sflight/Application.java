a0656f41cd08c5a87abdca8889ba765725ef44af
Robin Kuck <robin.kuck@sap.com>
2026-03-31 19:38:37 +0200

    Update Application.java

--- srv/src/main/java/com/sap/cap/sflight/Application.java
+++ srv/src/main/java/com/sap/cap/sflight/Application.java
@@ -2,9 +2,10 @@ package com.sap.cap.sflight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
 
 @SpringBootApplication
-@org.springframework.boot.web.servlet.ServletComponentScan
+@ServletComponentScan
 public class Application {
 
     public static void main(String[] args) {
