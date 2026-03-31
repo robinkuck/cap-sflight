5084caa7423844d5fa27f5ab09035c5f0e4f026f
Robin Kuck <robin.kuck@sap.com>
2026-03-31 20:15:07 +0200

    Revert "Update Application.java"
    This reverts commit a0656f41cd08c5a87abdca8889ba765725ef44af.

--- srv/src/main/java/com/sap/cap/sflight/Application.java
+++ srv/src/main/java/com/sap/cap/sflight/Application.java
@@ -2,10 +2,9 @@ package com.sap.cap.sflight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
 
 @SpringBootApplication
-@ServletComponentScan
+@org.springframework.boot.web.servlet.ServletComponentScan
 public class Application {
 
     public static void main(String[] args) {
